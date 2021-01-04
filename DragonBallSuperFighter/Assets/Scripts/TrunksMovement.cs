95e0c416987fd413443bc136d6a69e1ffedec59a
Shubh Sharma <flummoxedshubh@gmail.com>
2021-01-04 23:11:25 +0530

    Initial commit Re

---
+++ DragonBallSuperFighter/Assets/Scripts/TrunksMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrunksMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private Vector2 movement; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        anim.SetFloat("Horizontal", movement.x);
+
+        rb.MovePosition(new Vector2(movement.x * speed * Time.fixedDeltaTime, rb.position.y));
+    }
+}
